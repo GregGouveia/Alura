69201f8d51afdcc0ba4d32297b37806112bab5cd
Gregory Gouveia <gregorypgouveia@gmail.com>
2018-08-25 00:50:27 -0300

    Commit ContaCorrente

--- CSharp/CarreiraCSharp_02_CSharpPOO/ByteBank/01-ByteBank/Program.cs
+++ CSharp/CarreiraCSharp_02_CSharpPOO/ByteBank/01-ByteBank/Program.cs
@@ -4,12 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace _01_ByteBank
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //tipo        variavel          do tipo
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+            contaDaGabriela.saldo = 100;
+
+            Console.WriteLine("Titular: "+contaDaGabriela.titular);
+            Console.WriteLine("Agencia: "+ contaDaGabriela.agencia);
+            Console.WriteLine("Numero: "+contaDaGabriela.numero);
+            Console.WriteLine("Saldo: "+contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo += 200;
+            Console.WriteLine("Novo saldo: "+contaDaGabriela.saldo);
+
+            Console.ReadLine();
+
         }
     }
 }
